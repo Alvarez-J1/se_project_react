1473aadad5a234652eebdb14306104f2338f94bf
Joel Alvarez <Joel124@outlook.es>
2026-08-03 05:59:08 -0400

    a11y: label temperature unit switch

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -3,7 +3,7 @@ import "./ToggleSwitch.css";
 import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
 
 export default function ToggleSwitch() {
-  const { handleToggleSwitchChange } = useContext(
+  const { currentTemperatureUnit, handleToggleSwitchChange } = useContext(
     CurrentTemperatureUnitContext
   );
 
@@ -13,10 +13,16 @@ export default function ToggleSwitch() {
         onChange={handleToggleSwitchChange}
         type="checkbox"
         className="toggle-switch__checkbox"
+        checked={currentTemperatureUnit === "C"}
+        aria-label="Use Celsius temperature unit"
       />
       <span className="toggle-switch__circle"></span>
-      <span className="toggle-switch__text toggle-switch__text_F">F</span>
-      <span className="toggle-switch__text toggle-switch__text_C">C</span>
+      <span className="toggle-switch__text toggle-switch__text_F" aria-hidden="true">
+        F
+      </span>
+      <span className="toggle-switch__text toggle-switch__text_C" aria-hidden="true">
+        C
+      </span>
     </label>
   );
 }
