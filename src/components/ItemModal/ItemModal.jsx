cee47bdfcf9690edf6e06109af29c92d094083ff
Joel Alvarez <Joel124@outlook.es>
2026-08-03 05:57:08 -0400

    a11y: describe preview image with item name

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -15,7 +15,7 @@ function ItemModal({ activeModal, onClose, card, onDeleteItem }) {
           className="modal__close modal__close_type_image"
           aria-label="Close preview"
         ></button>
-        <img src={card.imageUrl} alt="image" className="modal__image" />
+        <img src={card.imageUrl} alt={card.name} className="modal__image" />
         <div className="modal__footer">
           <h2 className="modal__caption">{card.name}</h2>
           <p className="modal__weather">Weather: {card.weather}</p>
