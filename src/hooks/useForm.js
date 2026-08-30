c620ce84eb3c7b73af85e1cc003d2f7e5d827c64
Joel Alvarez <Joel124@outlook.es>
2026-08-30 04:23:43 -0400

    refactor: remove redundant form hook comments

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -4,9 +4,7 @@ export function useForm(inputValues) {
   const [values, setValues] = useState(inputValues);
 
   const handleChange = (event) => {
-    // get the name and value of the input because event.target is the input
     const { value, name } = event.target;
-    // set the value into the object using the name
     setValues((currentValues) => ({ ...currentValues, [name]: value }));
   };
   return { values, handleChange, setValues };
